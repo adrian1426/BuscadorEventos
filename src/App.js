9675f9b85b10ae6a2130f567c82916e0738ca903
adrian hernandez <adrianhh1426@gmail.com>
2019-11-24 00:02:54 -0600

    formulario de búsqueda

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
 import CategoriaProvider from './context/CategoriaContext';
+import Formulario from './components/Formulario';
 
 function App() {
   return (
     <CategoriaProvider>
       <Header/>
+      <div className="uk-container">
+        <Formulario/>
+      </div>
     </CategoriaProvider>
   );
 }
