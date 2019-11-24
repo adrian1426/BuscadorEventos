6e5aebab0a53e9695b074e6e7fa7fa0418ef089e
adrian hernandez <adrianhh1426@gmail.com>
2019-11-23 22:17:55 -0600

    header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () =>{
+    return(
+        <header className="uk-margin">
+            <h1 className="uk-text-center">Buscador de eventos AHH</h1>
+        </header>
+    );
+}
+
+export default Header;
